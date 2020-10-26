20778f324790a0711ba12a21902d7af596eba304
SueVerdi <verdyanss67@gmail.com>
2020-10-26 23:15:25 +0300

    lesson 1

---
+++ src/main/java/SueVerdi.java
@@ -0,0 +1,18 @@
+
+public class SueVerdi {
+
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 3000;
+        int c = 150000;
+        long d = 1700707;
+        float e = 11.11f;
+        double h = -222.333;
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+        System.out.println("c= " + c);
+        System.out.println("d= " + d);
+        System.out.println("e= " + e);
+        System.out.println("h= " + h);
+    }
+}
