3974f9ec1722e7457cb847199c4861756f4f5866
SueVerdi <verdyanss67@gmail.com>
2020-10-29 00:10:32 +0300

    lesson 1

--- src/main/java/SueVerdi.java
+++ src/main/java/SueVerdi.java
@@ -2,17 +2,66 @@
 public class SueVerdi {
 
     public static void main(String[] args) {
-        byte a = 100;
-        short b = 3000;
-        int c = 150000;
-        long d = 1700707;
-        float e = 11.11f;
-        double h = -222.333;
-        System.out.println("a= " + a);
-        System.out.println("b= " + b);
-        System.out.println("c= " + c);
-        System.out.println("d= " + d);
-        System.out.println("e= " + e);
-        System.out.println("h= " + h);
+        // Задание 3 ( Первые два задания сдала )
+        float a = 3.3f;
+        float b = 7.7f;
+        float c = 8.8f;
+        float d = 9.9f;
+        float f =  value(1, 4, 3, 6);
+        System.out.println(sumBetween10and20(10, 5));
+        System.out.println(f);
+        System.out.println();
+        PositiveOrNegative(10);
     }
+
+    public static float value(float a, float b, float c, float d) {
+        return a * (b + (c / d));
+    }
+    // Задание 4. Написать метод, принимающий на вход два целых числа и проверяющий,
+    // что сумма  лежит в пределах от 10 до 20 (включительно), если да - вернуть true,
+    // в противном случае  - false.
+
+    private static boolean sumBetween10and20(int a, int b) {
+        int sum = a + b;
+        if (sum <= 20 && sum >= 10) {
+            return true;
+        }
+        return false;
+    }
+    // Задание 5. Написать метод, которому в качестве параметра передается число, метод должен
+    // напечатать в консоль, положительное ли число передали или отрицательное.
+    // Замечание: ноль считаем положительным числом.
+
+    public static void PositiveOrNegative(int a) {
+        if (a >= 0) {
+            System.out.println("Положительное число");
+        }
+        if (a < 0) {
+            System.out.println("Отрицательное число");
+        }
+
+    }
+    // Задание 6. Написать метод, которому в качестве параметра передается целое число.
+    // Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+
+    private static boolean isNegative (int a) {
+        if (a<=0) {
+            return true;
+        }
+        return false;
+    }
+
+    // Задание 7. Написать метод, которому в качестве параметра передается строка,
+    // обозначающая имя. Метож должен вывести в консоль сообщение "Привет, указанное_имя!"
+
+    public static void greetings(String name) {
+        System.out.println("Привет, " + name + "!");
+    }
+
 }
+
+
+
+
+
+
