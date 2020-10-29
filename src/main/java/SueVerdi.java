170e5f15ff6171dff5e3999dc2738a462d8e8e9c
SueVerdi <verdyanss67@gmail.com>
2020-10-29 17:00:51 +0300

    lesson 1

--- src/main/java/SueVerdi.java
+++ src/main/java/SueVerdi.java
@@ -3,10 +3,6 @@ public class SueVerdi {
 
     public static void main(String[] args) {
         // Задание 3 ( Первые два задания сдала )
-        float a = 3.3f;
-        float b = 7.7f;
-        float c = 8.8f;
-        float d = 9.9f;
         float f =  value(1, 4, 3, 6);
         System.out.println(sumBetween10and20(10, 5));
         System.out.println(f);
@@ -23,10 +19,7 @@ public class SueVerdi {
 
     private static boolean sumBetween10and20(int a, int b) {
         int sum = a + b;
-        if (sum <= 20 && sum >= 10) {
-            return true;
-        }
-        return false;
+        return sum <= 20 && sum >= 10;
     }
     // Задание 5. Написать метод, которому в качестве параметра передается число, метод должен
     // напечатать в консоль, положительное ли число передали или отрицательное.
@@ -58,7 +51,8 @@ public class SueVerdi {
         System.out.println("Привет, " + name + "!");
     }
 
-}
+    }
+
 
 
 
